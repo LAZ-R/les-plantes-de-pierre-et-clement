c5a1a6003690d333595be63a223bf3b72fa3004c
LAZ-R <crazydrawer62@gmail.com>
2022-07-08 16:12:01 +0200

    v 1.0.2 : app versioning + dynamic copyright date

--- js/components/footer/footer.component.js
+++ js/components/footer/footer.component.js
@@ -1,9 +1,11 @@
+import * as SERVICE_PWA from '../../pwa.service.js'
+
 export const render = () => {
     const year = new Date().getFullYear();
     const footer = document.createElement('footer');
     footer.innerHTML =
         '<div>' +
-            '&copy; ' + year + ' - v 1.1 - <a href="https://laz-r.github.io/">laz_R</a> - <a href="./about.html">About</a>' +
+            '&copy; ' + year + ' - v' + SERVICE_PWA.getAppVersionNumber() + ' - <a href="https://laz-r.github.io/">laz_R</a> - <a href="./about.html">About</a>' +
         '</div>';
 
     document.getElementById('body').appendChild(footer);
